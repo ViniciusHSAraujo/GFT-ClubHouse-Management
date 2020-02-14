ffd3c06fb57f7700ffeb13326f6f2a176396f139
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-02-14 15:51:24 -0300

    Login/Register to MyAccount when the user is logged in

---
+++ GFT-ClubHouse- Management/Libs/Components/MenuUserControlViewComponent.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using GFT_ClubHouse__Management.Libs.Login;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GFT_ClubHouse__Management.Libs.Components {
+    public class MenuUserControlViewComponent: ViewComponent {
+        
+        private readonly LoginUser _loginUser;
+        
+        public MenuUserControlViewComponent(LoginUser loginUser) {
+            _loginUser = loginUser;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var user = _loginUser.GetUser();
+            return View(user);
+        }
+
+    }
+}
